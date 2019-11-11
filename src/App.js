c330b39a1facef6f413747dd9700c627f52d1483
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2019-11-10 21:32:48 -0500

    Updating demo page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import React from "react";
 import { Listbox } from "./components";
 
 const App = () => (
-  <>
-    <h1>Hello, World!!!</h1>
+  <div style={{ maxWidth: 600, margin: "0 auto" }}>
+    <h1>{"Currently showing the <Listbox> component"}</h1>
     <Listbox
-      options={[{ label: "one", value: 1 }, { label: "two", value: 2 }]}
+      options={[
+        { label: "one", value: 1 },
+        { label: "two", value: 2 },
+        { label: "three", value: 3 },
+        { label: "four", value: 4 },
+        { label: "five", value: 5 }
+      ]}
     />
-  </>
+  </div>
 );
 
 export default App;
